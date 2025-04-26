30831a30a3eea3ee8e33cf5d50ea8d11d9b8ae53
jasonhpham <jphamtihon@gmail.com>
2025-04-26 00:33:01 -0700

    Ramen Reply Basic Gameplay
    added logic for text, along with gameplay tracking
    TODO: implement art, transition to this and next minigame

---
+++ scripts/Ramen/ramen_reply.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var full_text = ""
+var full_text_length = 0
+@export var targetClicks = 20 #target clicks
+@onready var TextBody = $TextBody #get the richtextlabel object
+
+var currentClicks = 0
+
+func _ready() -> void:
+	#fetching the fulltext from the txt file
+	var file = FileAccess.open("res://Assets/misc/dummytext.txt", FileAccess.READ)
+	if file:
+		full_text = file.get_as_text()
+		file.close()
+		full_text_length = full_text.length()
+		TextBody.text = ""
+		#print(full_text_length)
+#mouse detection
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		currentClicks += 1
+		var charsToDisplay = (full_text_length/targetClicks) * currentClicks
+		# print(charsToDisplay)
+		var display = full_text.substr(0, charsToDisplay)
+		TextBody.text = "[color=black]" + display + "[/color]" #displays text
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+			if currentClicks >= targetClicks:
+				print("win")
+				#connect to next minigame here...
+
+#failure...
+func _on_timer_time_done() -> void:
+	SceneManager.change_scene_defeat()
