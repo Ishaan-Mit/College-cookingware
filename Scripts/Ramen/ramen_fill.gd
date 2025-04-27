c8c7f8f4170d8efd45e66493a67adef568bd5e92
Ishaan-Mit <Ishaan.Mittal07@gmail.com>
2025-04-27 05:20:16 -0700

    fill revamp

--- Scripts/Ramen/ramen_fill.gd
+++ Scripts/Ramen/ramen_fill.gd
@@ -1,18 +1,30 @@
 extends Node2D
 
-@onready var button: Button = $Button
+
 @onready var pan: Sprite2D = $Pan
+@onready var water_drag: HSlider = $WaterDrag
+@onready var pipe: Node2D = $Pipe
+@onready var particles: GPUParticles2D = $Pipe/GPUParticles2D
+
 var fill_level = 0
+var time_track = 0
 
 func _ready() -> void:
 	fill_level = 0
+	particles.emitting = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if button.button_pressed:
-		fill_level += 1
+func _process(delta: float) -> void:
+	time_track += delta
+	pan.position.x = 450 + 70 * sin(time_track)
+	pipe.position.x = 330+water_drag.value
+	
+	if particles.emitting and abs(pan.position.x-48-pipe.position.x) < 40:
+		fill_level += 0.5
 		SceneManager.play_sfx("res://Assets/audio/waterfill.wav")
-	pan.frame = min(4,int(fill_level/20))
+	
+	
+	pan.frame = min(4,int(fill_level/30))
 
 func _on_timer_time_done() -> void:
 	if pan.frame == 3:
@@ -20,6 +32,9 @@ func _on_timer_time_done() -> void:
 	else:
 		SceneManager.change_scene_defeat()
 
-func _on_button_button_up() -> void:
-	if pan.frame != 3:
-		fill_level += 5
+func _on_water_drag_drag_started() -> void:
+	particles.emitting = true
+
+
+func _on_water_drag_drag_ended(_value_changed: bool) -> void:
+	particles.emitting = false
