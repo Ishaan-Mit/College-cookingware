1bad05812ec6c6cbd0aadf3adb1ea4dce356cdb1
jasonhpham <jphamtihon@gmail.com>
2025-05-04 09:32:18 -0700

    fruit ninja and texting minigame

---
+++ Scripts/Nachos/fruit.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+@export var initialSpeed := Vector2.ZERO
+@export var half1: PackedScene
+@export var half2: PackedScene
+
+func _ready() -> void:
+	linear_velocity = initialSpeed
+
+func slice():
+	var half1 = half1.instantiate()
+	var half2 = half2.instantiate()
+	half1.global_position = global_position
+	half2.global_position = global_position
+	get_parent().add_child(half1)
+	get_parent().add_child(half2)
+	
+	var strength = 150                     
+	var dir1 = Vector2(randf_range(-1,1), randf_range(-1,1)).normalized()
+	var dir2 = Vector2(randf_range(-1,1), randf_range(-1,1)).normalized()
+
+	half1.apply_impulse(Vector2.ZERO, dir1 * strength)
+	half2.apply_impulse(Vector2.ZERO, dir2 * strength)
+	queue_free()
---
+++ Scripts/Nachos/nacho_ninja.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export var fruit_scenes: Array[PackedScene]
+@export var throwForce = 0
+@export var fruitsAtTime = 1
+@onready var spawnTimer = $SpawnTimer
+
+func _ready() -> void:
+	spawnTimer.start()
+
+func _on_spawn_timer_timeout() -> void:
+	for i in range(fruitsAtTime):
+		var fruit = fruit_scenes[randi() % fruit_scenes.size()].instantiate()
+		fruit.initialSpeed = Vector2(randf_range(-50,50), -1 * randf_range(throwForce-200,throwForce+100 ))
+		fruit.position = Vector2( randf_range(100, get_viewport_rect().size.x - 100), get_viewport_rect().size.y)
+		#print(fruit.position)
+		add_child(fruit)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body && body.name != "Onion" && body.get_script().resource_path.ends_with("fruit.gd") :
+		print("fruit" + body.name + "entered lose here")
+
+
+func _on_timer_time_done() -> void:
+	# print("win here")
+	return
