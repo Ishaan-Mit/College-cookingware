02555c5151695dba2f81189953c3472b250194a2
jasonhpham <jphamtihon@gmail.com>
2025-05-05 01:33:23 -0700

    added greybox fridge gameplay

---
+++ Scripts/Nachos/fridgebutton.gd
@@ -0,0 +1,4 @@
+extends TextureButton
+
+func _on_riffraff_pressed():
+	self.hide()
---
+++ Scripts/Nachos/nacho_fridge.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+@export var numObjects = 40
+@export var xLowerBound = 0
+@export var xUpperBound = 345
+@export var yLowerBound = 0
+@export var yUpperBound = 256
+
+@export var playAreas: Array[ColorRect]
+@onready var playArea = $PlayArea
+
+@export var avocado: TextureButton
+@export var buttonZ = 0
+
+@export var sampleButton: TextureButton
+
+func _ready() -> void:
+	randomize()
+	for i in numObjects: #make random rectangles
+		makeRect(i * 1.2, randi() % 2)
+	randomizeTBPosition(avocado,randi() % 2)
+	avocado.z_index = buttonZ
+
+func makeRect(z_index: float, playAreaIndex: int):
+	var rect = sampleButton.duplicate()
+	rect.mouse_filter = Control.MOUSE_FILTER_PASS 
+	var width = randf_range(36,100)
+	var height = randf_range(36,100)
+	rect.custom_minimum_size = Vector2(width, height)
+	
+	playAreas[playAreaIndex].add_child(rect)
+	var pos_x = randf_range(0, playAreas[playAreaIndex].size.x - width)
+	var pos_y = randf_range(0, playAreas[playAreaIndex].size.y - height)
+	rect.position = Vector2(pos_x, pos_y)
+	
+	#rect.add_color_override("font_color", Color(randf(), randf(), randf()))
+	
+	rect.z_index = z_index
+
+func randomizeTBPosition(object: TextureButton, playAreaIndex: int):
+	playAreas[playAreaIndex].add_child(object)
+	var pos_x = randf_range(0, playAreas[playAreaIndex].size.x)
+	var pos_y = randf_range(0, playAreas[playAreaIndex].size.y)
+	object.position = Vector2(pos_x, pos_y)
+	var console: String = str(playAreaIndex + 1) + " "+ str(pos_x) +  " " + str(pos_y)
+	print(console)
+	
+
+func _on_avocado_pressed() -> void:
+	SceneManager.play_sfx("res://Assets/audio/grabnew.wav")
+	print("win here")
+
+
+func _on_timer_time_done() -> void:
+	print("lose here")
