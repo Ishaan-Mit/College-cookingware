4b7f86991189ac01a09ef775129a4e5613ee7492
jasonhpham <jphamtihon@gmail.com>
2025-05-05 00:15:17 -0700

    finished logic for emojis, added some detail

--- Scripts/Nachos/nacho_emoji.gd
+++ Scripts/Nachos/nacho_emoji.gd
@@ -1,5 +1,32 @@
 extends Node2D
 
+@export var WrongEmojis: Array[CompressedTexture2D]
+@export var WrongButtons: Array[Button]
+@export var Buttons: Array[Button]
+@export var ButtonPositions: Array[Node2D]
+
+func _ready() -> void:
+	randomize()
+	#random sprites
+	var startIndex := randi() % WrongEmojis.size()
+	for i in WrongButtons:
+		var sprite:= i.get_node("Sprite") as Sprite2D
+		sprite.set_texture(WrongEmojis[++startIndex])
+		sprite.scale = Vector2(0.05, 0.05)
+		startIndex = (startIndex + 1) % WrongEmojis.size()
+	#random positions
+	randomize()
+	startIndex = randi() % Buttons.size()
+	for i in range(0, Buttons.size(), 1):
+		Buttons[i].position = ButtonPositions[startIndex].position
+		startIndex = (startIndex + 1) % ButtonPositions.size()
 
 func _on_timer_time_done() -> void:
-	SceneManager.change_scene("res://Scenes/Nachos/nacho_ninja.tscn", "chop!", 2)
+	print("lose here to time")
+	SceneManager.change_scene("res://Scenes/Nachos/nacho_ninja.tscn", "chop!", 2) #you should lose here
+
+func _on_heart_pressed() -> void:
+	print("win here")
+
+func _on_wrong_button_pressed() -> void:
+	print("lose here mistake made")
